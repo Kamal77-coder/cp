e82e5bf3b9d567b34a49964aba4e5eb3d8be7bbd
Kamal77-coder <63397944+Kamal77-coder@users.noreply.github.com>
2020-08-06 23:45:49 +0530

    Update basic string.cpp

--- basic string.cpp
+++ basic string.cpp
@@ -27,3 +27,50 @@ int main() {
     cout<<s2<<endl<<s3<<endl;
  
 }
+ //string basics part 2
+#include <iostream>
+
+#include<cstring>
+
+using namespace std;
+int main() {
+
+     string s1,s2;
+
+     s1 = "apple";
+     s2 = "mango";
+ 
+     cout<<s1.compare(s2)<<endl;  //-12 
+
+     cout<<s2.compare(s1)<<endl; //12
+
+     cout<<s1.compare(s1)<<endl; //0
+
+     //< and > are overloaded on strings
+ 
+      //finding substrings
+
+      string s6 = "I love apple and mang0";
+
+      int idx = s6.find("apple");  //starting index of word apple is 7
+      cout<<idx<<endl;
+
+      string word = "apple";
+      
+      int len = word.length();
+      s6.erase(idx , len);  
+
+      cout<<s6<<endl;  //I love  and mang0
+
+      //iterating all the characters of string
+
+      for(char ch : s6)
+      cout<<ch;
+
+      cout<<endl;
+
+      //iterating using iterators
+
+      for(auto it = s6.begin() ; it!= s6.end() ;it++)
+      cout<<*it;
+}
